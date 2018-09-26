a887fb150551e154c48534a43d16631c31ebebf3
Oniva <blackhoodyrap@gmail.com>
2018-09-26 17:44:09 -0400

    first commit

--- src/Inputs.js
+++ src/Inputs.js
@@ -3,7 +3,14 @@ import './App.css';
 import Button from '@material-ui/core/Button';
 
 
-let colors = ['#4DD0E1', '#9C27B0', '#E91E63', '#2196F3', '#FF5722', '#00838F', '#FF80AB'];
+
+
+let tasksList = []
+
+localStorage.setItem('tasks', JSON.stringify(tasksList));
+
+
+let colors = ['#4DD0E1', '#9C27B0', '#2196F3', '#FF5722', '#00838F', '#FF80AB'];
 
 let i = -1
 
@@ -14,19 +21,26 @@ class Inputs extends Component {
             editValueState: false,
             editText: "Edit",
         });
+        this.editValue = this.editValue.bind(this);
+        this.submitButton = this.submitButton.bind(this);
+    }
+
+    componentWillMount(){
+        for(let item in JSON.parse(localStorage.getItem('tasks'))){
+            this.submitButton(item);
+        }
+        
     }
 
     editValue(){
     
         if(this.state.editValueState === false)
         {
-            this.state.editValueState = true;
-            this.setState({ editText: "Stopped Edit"});
+            this.setState({ editValueState: true, editText: "Stop Editing" });
         }
         else
         {
-            this.editValueState = false;
-            this.setState({ editText: "Edit"});
+            this.setState({ editValueState: false, editText: "Edit" });
         }
     };
 
@@ -44,15 +58,28 @@ class Inputs extends Component {
         
     }
 
-    submitButton(){
+    submitButton(passedText=""){
         let para = document.createElement("p");
-        let text = document.createTextNode(document.getElementById("inputbox").value);
-        para.appendChild(text);
+        
+        if(passedText){
+            let text = document.createTextNode(document.getElementById("inputbox").value);
+            para.appendChild(text);
+            tasksList.push(document.getElementById("inputbox").value);
+            localStorage.setItem('items', JSON.stringify(tasksList));
+        }
+        else
+        {
+            let text = document.createTextNode(passedText);
+            para.appendChild(text);
+            tasksList.push(passedText);
+            localStorage.setItem('items', JSON.stringify(tasksList));
+        }
+        
     
       
     
         
-        // para.style.color = this.getColor();
+        para.style.color = this.getColor();
         
         para.style.textAlign = "center";
         document.body.appendChild(para);
@@ -66,12 +93,14 @@ class Inputs extends Component {
                 para.innerHTML = prompt("Please enter the edited task.", para.innerHTML);;
             }
         };
+        
+        
     };    
 
     render(){
         return(
         <div>
-            <Button className="edit" id="editbtn" onClick={this.editValue}>{this.editText}</Button>
+            <Button className="edit" id="editbtn" onClick={this.editValue}>{this.state.editText}</Button>
             <input id="inputbox" type="text" placeholder="New Task"></input>
             <Button id="submit" onClick={this.submitButton}>Submit</Button>
         </div>
